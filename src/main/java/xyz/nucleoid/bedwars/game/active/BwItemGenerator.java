7011235fd3992fc7eacba9dd8965bdfeb3dc6440
ZekeZ <contact@zekez.dev>
2026-02-11 18:22:47 +1000

    finish 1.21.11

--- src/main/java/xyz/nucleoid/bedwars/game/active/BwItemGenerator.java
+++ src/main/java/xyz/nucleoid/bedwars/game/active/BwItemGenerator.java
@@ -157,8 +157,13 @@ public final class BwItemGenerator {
             ItemStack stack = entity.getStack();
 
             player.giveItemStack(stack.copy());
-            player.networkHandler.sendPacket(entity.createSpawnPacket(new EntityTrackerEntry(world, entity, 1, true, (c) -> {}, (a, b) -> {})));
-            player.networkHandler.sendPacket(new ItemPickupAnimationS2CPacket(entity.getId(), player.getId(), stack.getCount()));
+            player.networkHandler.sendPacket(
+                        new ItemPickupAnimationS2CPacket(
+                                entity.getId(),
+                                player.getId(),
+                                stack.getCount()
+                        )
+            );
 
             player.getInventory().markDirty();
         }
--- src/main/java/xyz/nucleoid/bedwars/mixin/BedBlockMixin.java
+++ src/main/java/xyz/nucleoid/bedwars/mixin/BedBlockMixin.java
@@ -1,6 +1,10 @@
 package xyz.nucleoid.bedwars.mixin;
 
 import net.minecraft.block.BedBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,8 +20,8 @@ public class BedBlockMixin {
 	 *
 	 * @author SuperCoder79
 	 */
-	@Inject(method = "isBedWorking", at = @At("HEAD"), cancellable = true)
-	private static void noExplosion(World world, CallbackInfoReturnable<Boolean> cir) {
+	@Inject(method = "onUse", at = @At("HEAD"), cancellable = true)
+	private static void noExplosion(BlockState state, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit, CallbackInfoReturnable<Boolean> cir) {
 		var gameSpace = GameSpaceManager.get().byWorld(world);
 		if (gameSpace != null && gameSpace.getMetadata().sourceConfig().value().type() == BedWars.TYPE) {
 			cir.setReturnValue(true);
